19f53301debae69907bb22be96bc6864db2bac52
Chris Mathew Aje <chrismaje63@gmail.com>
2025-08-24 22:12:57 +0530

    Remove status fields from project entries in Projects component

--- src/components/Projects.tsx
+++ src/components/Projects.tsx
@@ -21,7 +21,6 @@ const Projects = () => {
       images: ['../../media/forms.thecma.xyz/forms.thecma (1).png', '../../media/forms.thecma.xyz/forms.thecma (2).png', '../../media/forms.thecma.xyz/forms.thecma (3).png', '../../media/forms.thecma.xyz/forms.thecma (4).png', '../../media/forms.thecma.xyz/forms.thecma (5).png', '../../media/forms.thecma.xyz/forms.thecma (6).png', '../../media/forms.thecma.xyz/forms.thecma (7).png'],
       link: 'https://forms.thecma.xyz/',
       github: null,
-      status: 'live',
       category: 'major'
     },
     {
@@ -36,7 +35,6 @@ const Projects = () => {
       images: ['../../media/ScanThePolicy/home.png', '../../media/ScanThePolicy/text_input.png', '../../media/ScanThePolicy/url_input.png'],
       link: null,
       github: 'https://github.com/chrismat-05/ScanThePolicy',
-      status: 'open-source',
       category: 'minor'
     },
     {
@@ -51,7 +49,6 @@ const Projects = () => {
       images: ['../../media/Quotzy/quotzy(1).png', '../../media/Quotzy/quotzy(2).png', '../../media/Quotzy/quotzy(3).png'],
       link: null,
       github: 'https://github.com/chrismat-05/quotzy',
-      status: 'open-source',
       category: 'minor'
     },
     {
@@ -66,7 +63,6 @@ const Projects = () => {
       images: ['../../media/scrihdr/scrihdr1.png', '../../media/scrihdr/scrihdr2.png', '../../media/scrihdr/scrihdr3.png'],
       link: 'https://scrihdr.thecma.xyz/',
       github: 'https://github.com/chrismat-05/SecuriHeader',
-      status: 'live',
       category: 'minor'
     },
     {
@@ -81,7 +77,6 @@ const Projects = () => {
       images: ['../../media/reqapi/reqapi1.png', '../../media/reqapi/reqapi2.png'],
       link: 'https://reqapi.thecma.xyz/',
       github: 'https://github.com/chrismat-05/Req-API',
-      status: 'live',
       category: 'minor'
     },
     {
@@ -96,7 +91,6 @@ const Projects = () => {
       images: ['../../media/IPLocator/1.png', '../../media/IPLocator/2.png'],
       link: 'https://iplocator.thecma.xyz/',
       github: 'https://github.com/chrismat-05/IPLocator',
-      status: 'live',
       category: 'minor'
     },
     {
@@ -111,7 +105,6 @@ const Projects = () => {
       images: ['../../media/AMC/AMC1.png', '../../media/AMC/AMC2.png', '../../media/AMC/AMC3.png', '../../media/AMC/AMC4.png', '../../media/AMC/AMC5.png', '../../media/AMC/AMC6.png', '../../media/AMC/AMC7.png', '../../media/AMC/AMC8.png'],
       link: null,
       github: 'https://github.com/chrismat-05/AMC',
-      status: 'open-source',
       category: 'college'
     },
   ];
